db4a2e7336f6dc2b559544a6a0194e4c46bcecba
Hano2001 <haraldnorlin93@gmail.com>
2024-01-20 18:40:48 +0100

    point change and coordinates

--- src/Pages/GamePage/GamePage.tsx
+++ src/Pages/GamePage/GamePage.tsx
@@ -1,46 +1,93 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Iplayers from "../../Components/Types";
 import { ContextInfo } from "../../Contexts/ContextInfo";
 import { numbers } from "../../Data/GameBoard";
 
 interface IboardItem {
-  ref: string;
+  tableCord: string;
   value: Number;
 }
 const GamePage = () => {
   const [boardItems, setBoardItems] = useState<IboardItem[]>([]);
-  function TableRow(props: { name: string; num?: number }) {
+  useEffect(() => {
+    players.map((player: Iplayers) => {
+      for (let i = 1; i <= 6; i++) {
+        let tableCord = `${player.id}:${i}`;
+        let newBoardItem = { tableCord: tableCord, value: 0 };
+        setBoardItems((prevBoard) => [...prevBoard, newBoardItem]);
+      }
+    });
+  }, []);
+
+  function TableRow(props: { name: string; num: number }) {
     return (
       <tr>
         <th>{props.name}</th>
         {players.map((player: Iplayers) => {
-          let ref = `${player.id}:${props.num}`;
-          let newBoardItem = { ref: ref, value: 0 };
-          setBoardItems(prevBoard =>[...prevBoard,newBoardItem])
-          return <td>0</td>;
+          let tableCord = `${player.id}:${props.num}`;
+          let obj = boardItems.find((item) => item.tableCord === tableCord);
+          let cellValue = obj?.value.toString();
+
+          return <td key={tableCord}>{cellValue}</td>;
         })}
       </tr>
     );
+  }
+  function ResultRow(props: { name: string }) {
+    return (
+      <>
+        <tr>
+          <th>{props.name}</th>
+
+          {players.map((player: Iplayers) => {
+            return <td key={player.id}>0</td>;
+          })}
+        </tr>
+      </>
+    );
+  }
+  function TestUpdateValue(e: any) {
+    e.preventDefault();
+    let cord = e.target.cord.value;
+    let amount = Number(e.target.amount.value);
+    let newArr = [...boardItems].map((item) => {
+      if (item.tableCord === cord) {
+        return { ...item, value: amount };
+      } else return item;
+    });
+    setBoardItems(newArr);
   }
   const { players } = useContext(ContextInfo);
   return (
     <div>
       <h1>gamepage</h1>
       <table>
-        <tr>
-          <th>Round</th>
-          {players.map((player: any) => {
-            return <th>{player.name}</th>;
-          })}
-        </tr>
+        <tbody>
+          <tr>
+            <th>Round</th>
+            {players.map((player: Iplayers) => {
+              return <th>{player.name}</th>;
+            })}
+          </tr>
 
-        {numbers.map((number) => {
-          return <TableRow name={number.name} num={number.number} />;
-        })}
-        <TableRow name="Score" />
-        <TableRow name="Bonus" />
+          {numbers.map((number) => {
+            return <TableRow name={number.name} num={number.number} />;
+          })}
+          <ResultRow name="Score" />
+          <ResultRow name="Bonus" />
+        </tbody>
       </table>
-      <button onClick={() => console.log(boardItems)}>TEST</button>
+      <form onSubmit={TestUpdateValue}>
+        <label>
+          Cord:
+          <input type="text" name="cord" />
+        </label>
+        <label>
+          Amount:
+          <input type="number" name="amount" />
+        </label>
+        <input type="submit" value="Update" />
+      </form>
     </div>
   );
 };
--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,41 +1,43 @@
-import React, { useContext, useState } from 'react'
-import Iplayers from '../../Components/Types';
-import { Link } from 'react-router-dom';
-import { ContextInfo } from '../../Contexts/ContextInfo';
+import React, { useContext, useState } from "react";
+import Iplayers from "../../Components/Types";
+import { Link } from "react-router-dom";
+import { ContextInfo } from "../../Contexts/ContextInfo";
 
 interface Props {}
 
 const HomePage = (props: Props) => {
-   const {players, setPlayers} = useContext(ContextInfo)
-    function handleSubmit(e: any) {
-      e.preventDefault();
-  
-      const newPlayer: Iplayers = { id: players.length, name: e.target.name.value };
-      setPlayers([...players, newPlayer]);
-    }
-    return (
-      <div className="App">
-        <h1>Yatzy Redemption</h1>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Player Name:
-            <input type="text" name="name" />
-          </label>
-          <input type="submit" value="Add Player"/>
-        </form>
-  
-        {players.map((player:any) => {
-          return (
-            <>
-              <h6>{player.name}</h6>
-              <p>{player.id}</p>
-            </>
-          );
-        })}
-        <button onClick={() => console.log(ContextInfo)}>TEST</button>
-        <Link to="/gamepage">Game</Link>
-      </div>
-    );
-}
+  const { players, setPlayers } = useContext(ContextInfo);
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    const newPlayer: Iplayers = {
+      id: players.length,
+      name: e.target.name.value,
+    };
+    setPlayers([...players, newPlayer]);
+    e.target.name.value = "";
+  }
+  return (
+    <div className="App">
+      <h1>Yatzy Redemption</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Player Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Add Player" />
+      </form>
 
-export default HomePage
+      {players.map((player: Iplayers) => {
+        return (
+          <div key={player.id}>
+            <h6>{player.name}</h6>
+            <p>{player.id}</p>
+          </div>
+        );
+      })}
+      <Link to="/gamepage">Game</Link>
+    </div>
+  );
+};
+
+export default HomePage;
